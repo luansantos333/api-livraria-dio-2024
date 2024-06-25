587898d419a7c97de46c54c943aee586839dd3e1
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-06-25 20:23:39 -0300

    Commit inicial

---
+++ src/test/java/me/dio/livraria/api_2024/ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.livraria.api_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
