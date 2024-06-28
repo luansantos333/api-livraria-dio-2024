f931f4fefca204d342522c1ef28bcb82d0afc90f
Luan Gomes dos Santos <luangomespctu@gmail.com>
2024-06-27 21:03:13 -0300

    OpenApiChange

--- src/main/java/me/dio/livraria/api_2024/repository/StudentReadListRepository.java
+++ src/main/java/me/dio/livraria/api_2024/repository/StudentReadListRepository.java
@@ -2,11 +2,6 @@ package me.dio.livraria.api_2024.repository;
 
 import me.dio.livraria.api_2024.model.StudentReadList;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-
-public interface CollegeReadListRepository extends CrudRepository <StudentReadList, Long> {
-
-
+public interface StudentReadListRepository extends CrudRepository <StudentReadList, Long> {
 }
